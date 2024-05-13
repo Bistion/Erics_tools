7ade253b767887e14dedff30ff167c143b2c882e
Joe <joseph.r.bechtold@gmail.com>
2024-05-13 15:44:17 -0400

    Update requirements.txt

--- app.py
+++ app.py
@@ -11,29 +11,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
 
-# SQLAlchemy database setup
-# load_dotenv()
-# TURSO_DATABASE_URL= os.getenv("TURSO_DATABASE_URL")
-# TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
-# dbUrl = f"sqlite+{TURSO_DATABASE_URL}/?authToken={TURSO_AUTH_TOKEN}&secure=true"
-# engine = create_engine(dbUrl, connect_args={'check_same_thread': False}, echo=True)
-#engine = create_engine(f"sqlite:///New_System_Scans.db", echo=False)
-
-# session = scoped_session(
-#   sessionmaker(
-#     autoflush=False,
-#     autocommit=False,
-#     bind=engine
-#   )
-# )
-
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
   cursor = dbapi_connection.cursor()
   cursor.execute("PRAGMA foreign_keys=ON")
   cursor.close()
-
-
 
 if __name__ == '__main__':
   app.run(debug=True, port=8000 )

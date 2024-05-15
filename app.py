70d956504204d369e5dd004ef4de5c1ca7569e41
Joe <joseph.r.bechtold@gmail.com>
2024-05-15 16:05:17 -0400

    trying max file age

--- app.py
+++ app.py
@@ -9,18 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix="/")
-
-@app.after_request
-def add_header(r):
-  """
-  Add headers to both force latest IE rendering engine or Chrome Frame,
-  and also to cache the rendered page for 10 minutes.
-  """
-  r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-  r.headers["Pragma"] = "no-cache"
-  r.headers["Expires"] = "0"
-  r.headers['Cache-Control'] = 'public, max-age=0'
-  return r
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 engine = create_engine(f"sqlite:////var/data/System_Scans.db", echo=False)
 

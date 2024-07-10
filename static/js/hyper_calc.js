61bb8c4d80c00115a7aee9a06ac07a9437c7a1cb
Joe <joseph.r.bechtold@gmail.com>
2024-07-09 22:45:47 -0400

    Bugfix on dropdowns

--- static/js/hyper_calc.js
+++ static/js/hyper_calc.js
@@ -50,29 +50,6 @@ function draw(pathdisplayJson) {
 
 function setCanvasSize(canvas) {
   var rect = canvas.parentNode.getBoundingClientRect();
-  console.log(rect)
   canvas.width = rect.width;
   canvas.height = rect.height;
-}
-
-function generatePoints(sectorList,systemList) {
-  var secJson = JSON.parse(sectorList);
-  var sectorOptions = "<option></option>";
-  for (var i = 0; i < secJson.length; i++) {
-    sectorOptions += "<option>" + secJson[i].sector_name + "</option>";
-  }
-  document.getElementById("sectorStartName").innerHTML = sectorOptions;
-  document.getElementById("sectorEndName").innerHTML = sectorOptions;
-  
-  var pathElement = "";
-  for (var i = 0; i < pathdisplayJson.length; i++) {
-    let x = Math.max(-500, Math.min(500, pathdisplayJson[i].x_coord));
-    let y = Math.max(-500, Math.min(500, pathdisplayJson[i].y_coord));
-    let newX = 500 + x;
-    let newY = 500 - y;
-    let sysID = pathdisplayJson[i].system_uid.replace("9:","")
-    let sysHref = "https://www.swcombine.com/rules/?Galaxy_Map&systemID=" + sysID
-    pathElement = pathElement += '<a class="combatplanet" style="position:absolute;z-index:2;top:' + newY + 'px; left:' + newX + 'px" title="Sector: ' + pathdisplayJson[i].sector_name + '<br>System: '+ pathdisplayJson[i].system_name + ' (' + y + ', ' + x + ')" href="' + sysHref + '" target="_blank"></a>';
-  }
-  document.getElementById("mapVals").innerHTML = pathElement;
 }
